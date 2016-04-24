b2f0be60a85b2718d3888c717b2a4f8ef22de114
paulszh <paulszh@pauslzhtekiMacBook-Pro.local>
2016-04-24 16:32:04 -0700

    First commit

---
+++ linkXcodeWithGithub/linkXcodeWithGithub/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  linkXcodeWithGithub
+//
+//  Created by paulszh on 4/24/16.
+//  Copyright © 2016 pauslzh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("first commit to the github")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
